001bb5d9cbf3039535cd56a875136b08bb7bf7e9
xub <xubo@zjport.gov.cn>
2021-05-13 17:34:26 +0800

    网关部分实现

---
+++ mall-gatway/src/main/java/com/jincou/getway/GatWayApplication.java
@@ -0,0 +1,20 @@
+package com.jincou.getway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+/**
+ *  网管启动类
+ *
+ * @author xub
+ * @date 2021/3/30 下午3:35
+ */
+@SpringBootApplication
+public class GatWayApplication {
+
+    public static void main(String [] args){
+        SpringApplication.run(GatWayApplication.class,args);
+    }
+
+}
